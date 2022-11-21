a5f4813febae97b954fed08d99f0af162b8d5563
Nova Casserfelt <casserfelt.nova@elev.ga.ntig.se>
2022-11-21 10:39:59 +0100

    hej

--- src/Bank.java
+++ src/Bank.java
@@ -1,13 +1,35 @@
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bank {
 
     //Attributes
-    ArrayList<String> ustomers = new ArrayList<>();
-    ArrayList<Integer> accounts = new ArrayList<>();
+    ArrayList<Customer> customers = new ArrayList<>();
+    ArrayList<Account> accounts = new ArrayList<>();
+    Scanner myScan = new Scanner(System.in);
 
     //Constructors
-    public Bank(){}
+    public Bank(){
+        System.out.println("Do you want to create a new user? (1: yes, 2: no)");
+
+        while (true) {
+
+            try {
+                int ans = myScan.nextInt();
+
+                if (ans == 2) {
+                    System.out.println("no new user");
+                }
+                break;
+          //  } catch (IOException e) {
+          //      System.out.println("not a number");
+            } catch (Exception e) {
+                System.out.println();e.getMessage();
+                System.out.println("Something went worng!");
+            }
+        }
+    }
 
     //Methods
 
